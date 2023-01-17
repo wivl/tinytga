ab73378e8351b569f86c58460b9d156a5ccbc7fa
wivl <68458084+wivl@users.noreply.github.com>
2023-01-18 02:33:00 +0800

    Update tinytga.h

--- tinytga.h
+++ tinytga.h
@@ -1,7 +1,24 @@
 #ifndef _TINY_TGA_H
 #define _TINY_TGA_H
 
+#include <stdint.h>
 
+
+struct tt_header {
+	uint8_t 
+}
+
+struct tt_color {
+	uint8_t a;
+	uint8_t b;
+	uint8_t g;
+	uint8_t r;
+};
+
+
+struct tt_image {
+	tt_color *color_map;
+};
 
 
 

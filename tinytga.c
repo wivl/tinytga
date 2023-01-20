671c9be9abca9175d2428eb50d5deeede6914103
wivl <68458084+wivl@users.noreply.github.com>
2023-01-20 18:21:47 +0800

    complete tga header

--- tinytga.c
+++ tinytga.c
@@ -1,3 +1,51 @@
 #include "tinytga.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define HEADER_LENGTH 18
+
+void dev_log(tt_image* image) {
+	if (image == NULL) {
+		fprintf(stderr, "[DEV]Image pointer value is NULL\n");
+		exit(1);
+	}
+	printf("[DEV]Header\n");
+	printf("[DEV]Field 1:\t\t%u\n", image->header.id_length);
+	printf("[DEV]Field 2:\t\t%u\n", image->header.color_map_type);
+	printf("[DEV]Field 3:\t\t%u\n", image->header.image_type);
+	printf("[DEV]Field 4:\t\t");
+	for(int i = 0; i < 5; i++) { printf("%u ", image->header.color_map_specification[i]); }
+	puts("");
+	printf("[DEV]Field 5:\t\t");
+	for(int i = 0; i < 10; i++) { printf("%u ", image->header.image_specification[i]); }
+	puts("");
+}
+
+tt_image* tt_load_from_file(const char *file_path) {
+	FILE *image_stream = fopen(file_path, "rb");
+	if (image_stream == NULL) {
+		fprintf(stderr, "[ERR]File %s not found\n", file_path);
+		exit(1);
+	}
+	tt_image *image = (tt_image*)malloc(sizeof(tt_image));
+	// 18 bytes header
+	uint8_t header[18];
+	fread(header, sizeof(uint8_t), HEADER_LENGTH, image_stream);
+	image->header.id_length = header[0];
+	image->header.color_map_type = header[1];
+	image->header.image_type = header[2];
+	image->header.color_map_specification = (uint8_t*)malloc(5); // the length of field 4 is 5 bytes
+	for (int i = 0; i < 5; i++) { image->header.color_map_specification[i] = header[i+3]; }
+	image->header.image_specification = (uint8_t*)malloc(10);	 // the length of field 5 is 10 bytes
+	for (int i = 0; i < 10; i++) { image->header.image_specification[i] = header[i+8]; }
+
+	dev_log(image);
+
+	// ID, color map and pixels
+	fread(image->image_id, sizeof(uint8_t), image->header.id_length, image_stream);
 
 
+	fclose(image_stream);
+	return image;
+}
--- tinytga.h
+++ tinytga.h
@@ -4,22 +4,33 @@
 #include <stdint.h>
 
 
-struct tt_header {
-	uint8_t 
-}
+typedef struct {
+	uint8_t id_length;				  // Length of the image ID field (0-255)
+	uint8_t color_map_type;			  // Whether a color map is included
+	uint8_t image_type;				  // Compression and color types
+	uint8_t *color_map_specification; // 5 bytes, describes the color map
+	uint8_t *image_specification;	  // 10 bytes, image dimensions and format
+} tt_header;
 
-struct tt_color {
+typedef struct {
 	uint8_t a;
 	uint8_t b;
 	uint8_t g;
 	uint8_t r;
-};
+} tt_color;
 
 
-struct tt_image {
+typedef struct {
+	tt_header header;
+	uint8_t *image_id;
+	uint16_t width;
+	uint16_t height;
 	tt_color *color_map;
-};
+	uint32_t *pixels;
+} tt_image;
 
+/* load tga image from file */
+tt_image* tt_load_from_file(const char* file_path);
 
 
 
